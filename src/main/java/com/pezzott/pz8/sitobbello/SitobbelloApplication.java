1288fd32aeaf473713b417ad5c2ced8b7e0e8797
Angelo <pezzottaggine@gmail.com>
2023-02-10 12:48:31 +0100

    First commit

---
+++ src/main/java/com/pezzott/pz8/sitobbello/SitobbelloApplication.java
@@ -0,0 +1,13 @@
+package com.pezzott.pz8.sitobbello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitobbelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitobbelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pezzott/pz8/sitobbello/SitobbelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pezzott.pz8.sitobbello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*@SpringBootTest
+class SitobbelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
